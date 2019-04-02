56f26823617bbd7e4c8078cf2a334112406b5ce3
Jared Hanson, Pavel Zanchuk and Tristan Setha <jared@>
2019-04-02 10:44:08 -0700

    functioning recipebox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'super-recipe',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Recipe box';
+  recipies: Recipe[] = [
+    new Recipe('cookies', ['flour','milk','eggs', 'chocolate'], ['mix everything together', 'bake at 350 degrees for 12 minutes']),
+    new Recipe('rice noodles', ['flour','water','starch'], ['mix everything together', 'put through noodle press', 'boil for 15 minutes on med temp']),
+    new Recipe('french fries', ['potato',' salt','water', 'oil'], ['cut potatos do desires length', 'soak potatoes in salt water for 24 hours', 'fire it ON'])
+
+  ]
+
 }
